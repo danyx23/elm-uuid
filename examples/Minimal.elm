392187760768aa91f6d314efad9f965577dfced4
Daniel Bachler <daniel@danielbachler.de>
2016-05-16 14:21:47 +0200

    Updated examples to 0.17

--- examples/Minimal.elm
+++ examples/Minimal.elm
@@ -1,59 +1,78 @@
-module MinimalExample 
-  ( main
-  )
-  where
+module MinimalExample
+    exposing
+        ( main
+        )
 
 import Uuid
-import Random.PCG exposing (Seed, initialSeed2, generate)
-import StartApp.Simple exposing (start)
+import Random.Pcg exposing (Seed, initialSeed2, step)
+import Html.App exposing (programWithFlags)
 import Html exposing (Html, div, button, text)
 import Html.Events exposing (onClick)
 
--- 1.: Plumbing code to get a good initial random seed from Javascript via a port
---     (As described in the docs of mgold/elm-random-pcg) 
--- Elm
-port randomSeed : (Int, Int)
 
-seed0 : Seed
-seed0 = (uncurry initialSeed2) randomSeed
- 
--- 2.: In your elm code, store the seed and update it every time you create a new Uuid
-type alias Model = 
-  { currentSeed : Seed
-  , currentUuid : Maybe Uuid.Uuid
-  }
+-- 1.: In your elm code, store the seed and update it every time you create a new Uuid
 
-type Action = NewUuid
 
-update : Action -> Model -> Model
-update action model =
-  case action of 
-    NewUuid ->
-      let
-        (newUuid, newSeed) = generate Uuid.uuidGenerator model.currentSeed
-      in
-      { model
-      | currentUuid = Just newUuid
-      , currentSeed = newSeed 
-      }
+type alias Model =
+    { currentSeed : Seed
+    , currentUuid : Maybe Uuid.Uuid
+    }
 
-view : Signal.Address Action -> Model -> Html      
-view address model =
-  let
-    uuidText = case model.currentUuid of
-                Nothing -> 
-                  "No Uuid was created so far"
+
+type Msg
+    = NewUuid
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NewUuid ->
+            let
+                ( newUuid, newSeed ) =
+                    step Uuid.uuidGenerator model.currentSeed
+            in
+                ( { model
+                    | currentUuid = Just newUuid
+                    , currentSeed = newSeed
+                  }
+                , Cmd.none
+                )
+
+
+view : Model -> Html Msg
+view model =
+    let
+        uuidText =
+            case model.currentUuid of
+                Nothing ->
+                    "No Uuid was created so far"
+
                 Just uuid ->
                     "Current Uuid: " ++ Uuid.toString uuid
-  in
-    div []
-      [ button [ onClick address NewUuid ] [ text "Create a new Uuid!" ]
-      , text uuidText          
-      ]
-    
+    in
+        div []
+            [ button [ onClick NewUuid ] [ text "Create a new Uuid!" ]
+            , text uuidText
+            ]
+
+{- this init function takes a tuple of two ints that are handed over
+in the initializiation code of our Elm app in the javascript code. It
+uses these JS random values as the initial seed.
+-}
+init : ( Int, Int ) -> ( Model, Cmd Msg )
+init ( seed1, seed2 ) =
+    ( { currentSeed = initialSeed2 seed1 seed2
+      , currentUuid = Nothing
+      }
+    , Cmd.none
+    )
+
+
+main : Program ( Int, Int )
 main =
-  start
-    { model = { currentSeed = seed0, currentUuid = Nothing }
-    , update = update
-    , view = view
-    }
+    programWithFlags -- using programWithFlags to get the seed values from JS
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = \_ -> Sub.none
+        }
